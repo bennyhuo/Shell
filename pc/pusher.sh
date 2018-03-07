624563159a28d16ebde5c6dad8ccaacd4089f270
enbandari <305779913@qq.com>
2018-03-07 11:55:33 +0800

    提交脚本，主要解决多个设备连接的问题。这个不是最终方案。可能需要添加插件支持效果更好

--- pc/pusher.sh
+++ pc/pusher.sh
@@ -4,13 +4,13 @@ echo $PATH
 #取脚本所在目录
 dir=$(cd `dirname $0`; pwd)
 
-adb push $dir/server.dex  /data/local/tmp
-adb push $dir/launcher.sh  /data/local/tmp
-adb push $dir/shellserver  /data/local/tmp
+$dir/eadb push $dir/server.dex  /data/local/tmp
+$dir/eadb push $dir/launcher.sh  /data/local/tmp
+$dir/eadb push $dir/shellserver  /data/local/tmp
 
 # 不输出 后台执行
 # 注意，第一个 nohup ... &是为了不阻塞电脑；第二个 nohup 是为了不让 Android 在 USB 断开后杀掉这个进程。
 # 第二个 nohup 如果后面加 &，oppo R9S 服务会在执行完命令立即关掉，奇怪
 # >/dev/null 会卡住 Gradle，&>/dev/null Gradle 任务执行完后会退出（对于 Mac Terminal 这两个效果一样）
-nohup adb shell "nohup /data/local/tmp/launcher.sh > /dev/null 2>&1" &>/dev/null &
+nohup $dir/eadb shell "nohup /data/local/tmp/launcher.sh > /dev/null 2>&1" &>/dev/null &
 echo 'SUCCESSFUL'
